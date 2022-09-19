fef831b85467c8f4585e30dded5e56febcf520b0
Thomas Peterson <trp46@uclive.ac.nz>
2022-09-20 04:27:09 +1200

    Tidy up transformation matrices for each station

--- coffee_maker.py
+++ coffee_maker.py
@@ -3,7 +3,9 @@ import robodk as rdk     # Robot toolbox
 import numpy as np
 import math as math
 
-# -------------Setup Robot --------------- #
+""" ---------------------------------------------- """
+""" ---------------- Setup Robot ----------------- """
+""" ---------------------------------------------- """
 RDK = rl.Robolink()
 
 robot = RDK.Item('UR5')
@@ -12,16 +14,12 @@ target = RDK.Item('Home')   # existing target in station
 robot.setPoseFrame(world_frame)
 robot.setPoseTool(robot.PoseTool())
 
-# ------------- Define Functions --------------- #
+""" ---------------------------------------------- """
+""" ------------- Define Functions --------------- """
+""" ---------------------------------------------- """
 def testMove(transform):
     robot.MoveJ(T_home, blocking=True)
-    robot.MoveJ(transform)
-    # robot.MoveJ(T_home, blocking=True)
-
-def attachCup():
-    robot.MoveJ(target, blocking=True)
-    robot.MoveJ(T_cup_stand, blocking=True)
-    robot.MoveJ(target, blocking=True)
+    robot.MoveJ(rdk.Mat(transform.tolist()))
 
 def normalise(v):
     norm = np.linalg.norm(v)
@@ -29,7 +27,6 @@ def normalise(v):
        return v
     return v / norm
 
-# Checks if a matrix is a valid rotation matrix.
 def isRotationMatrix(R) :
     Rt = np.transpose(R)
     shouldBeIdentity = np.dot(Rt, R)
@@ -37,9 +34,6 @@ def isRotationMatrix(R) :
     n = np.linalg.norm(I - shouldBeIdentity)
     return n < 1e-6
 
-# Calculates rotation matrix to euler angles
-# The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
 def rotationMatrixToEulerAngles(R) :
 
     assert(isRotationMatrix(R))
@@ -59,19 +53,18 @@ def rotationMatrixToEulerAngles(R) :
 
     return np.array([x, y, z])
 
-
-# ------------- Define variables --------------- #
+""" ---------------------------------------------- """
+""" ------------- Define variables --------------- """
+""" ---------------------------------------------- """
 xDir = np.array([1, 0, 0])
 yDir = np.array([0, 1, 0])
 zDir = np.array([0, 0, 1])
 
 # Directly use the RDK Matrix object from to hold pose (its an HT)
-T_home = rdk.Mat([[     0.000000,     0.000000,     1.000000,   523.370000 ],
-     [-1.000000,     0.000000,     0.000000,  -109.000000 ],
-     [-0.000000,    -1.000000,     0.000000,   607.850000 ],
-      [0.000000,     0.000000,     0.000000,     1.000000 ]])
-
-robot.MoveJ(T_home, blocking=True)
+T_home = rdk.Mat([[ 0.000000,     0.000000,     1.000000,   523.370000 ],
+                  [-1.000000,     0.000000,     0.000000,  -109.000000 ],
+                  [-0.000000,    -1.000000,     0.000000,   607.850000 ],
+                  [ 0.000000,     0.000000,     0.000000,     1.000000 ]])
 
 # ------------- Tamp Stand --------------- #
 ts_ref_g    = np.array([600.1, 52.8, 254.5])
@@ -81,19 +74,21 @@ ts_pointY_g = np.array([678.4, 70.7, 250.5])
 ts_xDir_g = normalise(ts_ref_g - ts_pointX_g)
 ts_yDir_g = normalise(ts_pointY_g - ts_ref_g)
 
-# Different rotation matrix technique
+# Z Rotation
 ts_cAlpha = np.cos(-76 * np.pi/180)
 ts_sAlpha = np.sin(-76 * np.pi/180)
 ts_zRot = np.array([[ts_cAlpha, -1 * ts_sAlpha, 0],
                     [ts_sAlpha,      ts_cAlpha, 0],
                     [        0,              0, 1]])
 
+# Y Rotation
 ts_cBeta = np.cos(-6.3 * np.pi/180)
 ts_sBeta = np.sin(-6.3 * np.pi/180)
 ts_yRot = np.array([[     ts_cBeta, 0, ts_sBeta],
                     [            0, 1,        0], 
                     [-1 * ts_sBeta, 0, ts_cBeta]])
 
+# X Rotation
 ts_cGamma = np.cos(-10 * np.pi/180)
 ts_sGamma = np.sin(-10 * np.pi/180)
 ts_xRot = np.array([[1,         0,              0],
@@ -102,45 +97,65 @@ ts_xRot = np.array([[1,         0,              0],
 
 ts_Rot = np.matmul(np.matmul(ts_zRot, ts_yRot), ts_xRot)
 
-T_ur_ts = rdk.Mat(np.append(np.c_[ts_Rot.tolist(), ts_ref_g.transpose().tolist()], np.array([[0, 0, 0, 1]])).tolist())
-
-# testMove(T_ur_ts) # Move to reference from home and return to home
+T_tamp_stand = np.append(np.c_[ts_Rot.tolist(), ts_ref_g.transpose().tolist()], np.array([[0, 0, 0, 1]]))
 
 # ------------- Cup Stand --------------- #
-T_cup_stand = np.array([[    -1.000000,     0.000000,     0.000000,   -1.5000000 ],
-     [0.000000,    -1.000000,     0.000000,  -600.800000 ],
-     [0.000000,     0.000000,     1.000000,   -20.000000 ],
-     [0.000000,     0.000000,     0.000000,     1.000000 ]])
+cs_ref_g = np.array([-1.5, -600.8, -20])
 
-T_cup_stand = rdk.Mat(T_cup_stand.tolist())
+T_cup_stand = np.array([[-1.000000,     0.000000,     0.000000,    cs_ref_g[0]],
+                        [ 0.000000,    -1.000000,     0.000000,    cs_ref_g[1]],
+                        [ 0.000000,     0.000000,     1.000000,    cs_ref_g[2]],
+                        [ 0.000000,     0.000000,     0.000000,       1.000000]])
 
-# testMove(T_cup_stand)
+# ----------- Coffee Grinder ------------- #
+cg_point_g = np.array([370.1, -322.3, 317.3])
+cg_ref_g   = np.array([482.7, -434.3, 317.3])
+cg_diff    = normalise(cg_point_g - cg_ref_g)
 
-# ------------- Grinder --------------- #
-cg_p_g = np.array([370.1, -322.3, 317.3])
-cg_ref_g = np.array([482.7, -434.3, 317.3])
-cg_diff = normalise(cg_p_g - cg_ref_g)
+cg_sTheta = np.dot(cg_diff, yDir)
+cg_cTheta = -1 * np.dot(cg_diff, yDir)
 
-T_grinder = np.array([[    cg_diff[0],     -cg_diff[1],     0.000000,   482.700000 ],
-     [cg_diff[1],    cg_diff[0],     0.000000,  -434.300000 ],
-     [0.000000,     0.000000,     1.000000,   317.300000 ],
-     [0.000000,     0.000000,     0.000000,     1.000000 ]])
+T_grinder = np.array([[ cg_cTheta, -1 * cg_sTheta,     0.000000,   cg_ref_g[0] ],
+                      [ cg_sTheta,      cg_cTheta,     0.000000,   cg_ref_g[1] ],
+                      [  0.000000,       0.000000,     1.000000,   cg_ref_g[2] ],
+                      [  0.000000,       0.000000,     0.000000,      1.000000 ]])
 
-T_grinder = rdk.Mat(T_grinder.tolist())
-
-# testMove(T_grinder)
 
 # ------------- Coffee Machine --------------- #
-cm_p_g = np.array([-580.4, -444.7, 350.6])
-cm_ref_g = np.array([-368.4, -389.0, 350.6])
-cm_diff = normalise(cm_p_g - cm_ref_g)
+cm_point_g = np.array([-580.4, -444.7, 350.6])
+cm_ref_g   = np.array([-368.4, -389.0, 350.6])
+cm_diff    = normalise(cm_point_g - cm_ref_g)
 
+cm_cTheta = np.dot(cg_diff, yDir)
+cm_sTheta = -1 * np.dot(cg_diff, xDir)
 
-T_machine = np.array([[    cm_diff[1],     cm_diff[0],     0.000000,   -368.400000],
-     [-cm_diff[0],    cm_diff[1],     0.000000,  -389.000000 ],
-     [0.000000,     0.000000,     1.000000,   350.600000 ],
-     [0.000000,     0.000000,     0.000000,     1.000000 ]])
+T_machine = np.array([[ cm_cTheta, -1 * cm_sTheta,     0.000000,   cm_ref_g[0]],
+                      [ cm_sTheta,      cm_cTheta,     0.000000,   cm_ref_g[1]],
+                      [ 0.0000000,        0.000000,    1.000000,   cm_ref_g[2]],
+                      [ 0.0000000,        0.000000,    0.000000,      1.000000]])
 
+# ------------- Tool stand --------------- #
+alpha_bt = np.arctan(1)-np.arctan(89.1/155.9)
+T_tool_stand = np.array([[    np.cos(alpha_bt),     np.sin(alpha_bt),     0.000000,   -556.500000 ],
+                        [-np.sin(alpha_bt),    np.cos(alpha_bt),     0.000000,  -77.400000 ],
+                        [0.000000,     0.000000,     1.000000,   19.050000 ],
+                        [0.000000,     0.000000,     0.000000,     1.000000 ]])
+
+# ts_point_g = np.array([-645.7, 78.5, 19.05])
+# ts_ref_g   = np.array([-556.5, -77.4, 19.05])
+# ts_diff    = normalise(ts_point_g - ts_ref_g)
+
+# ts_cTheta = np.dot(cg_diff, yDir)
+# ts_sTheta = -1 * np.dot(cg_diff, xDir)
+
+# T_machine = np.array([[    ts_diff[0],     -ts_diff[1],     0.000000,   -556.500000],
+#      [ts_diff[1],    ts_diff[0],     0.000000,  -77.400000 ],
+#      [0.000000,     0.000000,     1.000000,   19.050000 ],
+#      [0.000000,     0.000000,     0.000000,     1.000000 ]])
+
+""" ---------------------------------------------- """
+""" ----------- -- Define Motion  - -------------- """
+""" ---------------------------------------------- """
 T_machine_but_b = np.array([[    0.0,     0.0,     -1.000000,   70.67],
      [0.0,    1.0,     0.000000,  35.25],
      [1.000000,     0.000000,     0.000000,   -27.90 ],
@@ -148,27 +163,7 @@ T_machine_but_b = np.array([[    0.0,     0.0,     -1.000000,   70.67],
 
 
 tRaNsFoRm = np.matmul(T_machine,T_machine_but_b)
-tRaNsFoRm = rdk.Mat(tRaNsFoRm.tolist())
-# T_machine = rdk.Mat(T_machine.tolist())
 
-testMove(tRaNsFoRm)
+robot.MoveJ(T_home, blocking=True)
 
-# ------------- Tool stand --------------- #
-alpha_bt = np.arctan(1)-np.arctan(89.1/155.9)
-T_tool_stand = np.array([[    np.cos(alpha_bt),     np.sin(alpha_bt),     0.000000,   -556.500000 ],
-     [-np.sin(alpha_bt),    np.cos(alpha_bt),     0.000000,  -77.400000 ],
-     [0.000000,     0.000000,     1.000000,   19.050000 ],
-     [0.000000,     0.000000,     0.000000,     1.000000 ]])
-
-# ts_p_g = np.array([-645.7, 78.5, 19.05])
-# ts_ref_g = np.array([-556.5, -77.4, 19.05])
-# ts_diff = normalise(ts_p_g - ts_ref_g)
-
-# T_machine = np.array([[    ts_diff[0],     -ts_diff[1],     0.000000,   -556.500000],
-#      [ts_diff[1],    ts_diff[0],     0.000000,  -77.400000 ],
-#      [0.000000,     0.000000,     1.000000,   19.050000 ],
-#      [0.000000,     0.000000,     0.000000,     1.000000 ]])
-
-T_tool_stand = rdk.Mat(T_tool_stand.tolist())
-
-# testMove(T_tool_stand)
+testMove(T_tool_stand)
